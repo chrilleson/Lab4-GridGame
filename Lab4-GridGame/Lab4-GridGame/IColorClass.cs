42cdc4675f0d03d5daa1c08b5d474f444f52b86d
Knut <knut@lyven.se>
2017-10-03 15:01:36 +0200

    Created IColorClass Interface

---
+++ Lab4-GridGame/Lab4-GridGame/IColorClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_GridGame
+{
+    //Gives color to the output.
+    interface IColorClass
+    {
+        void Color();
+    }
+}
