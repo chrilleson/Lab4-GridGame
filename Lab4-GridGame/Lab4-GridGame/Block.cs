e9161cf5007396d0146768074ceb2192ea09bf50
chrilleson <Christoffer.Ljungqvist@hotmail.se>
2017-10-03 13:53:38 +0200

    Created Map.cs and Block.cs

---
+++ Lab4-GridGame/Lab4-GridGame/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_GridGame
+{
+    public abstract class Block
+    {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+    }
+}
