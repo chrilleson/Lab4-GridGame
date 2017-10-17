1b9344dce8523fe793e452b438ff33bc4ff218a4
chrilleson <Christoffer.Ljungqvist@hotmail.se>
2017-10-17 13:51:02 +0200

    Added a troll

--- Lab4-GridGame/Lab4-GridGame/Map.cs
+++ Lab4-GridGame/Lab4-GridGame/Map.cs
@@ -61,7 +61,7 @@ namespace Lab4_GridGame
                     {
                         if (col == 0 || col == MapGrid.GetLength(1) - 1 || row == 0 || row == MapGrid.GetLength(0) - 1 ||
                             col == 2 && row == 3 || col == 2 && row == 2 || col == 12 && row == 3 || col == 12 && row == 2 ||
-                            col == 6 && row == 1 || col == 8 && row == 1 || col == 6 && row == 3 || col == 8 && row == 3 || row == 5 && col != 5 && col != 16 || col == 15 || col == 14 && row == 1 )
+                            col == 6 && row == 1 || col == 8 && row == 1 || col == 6 && row == 3 || col == 8 && row == 3 || row == 5 && col != 5 && col != 16 || col == 15 && row >= 3)
                         {
                             MapGrid[row, col] = new Wall();
                             Console.Write(' ');
@@ -69,6 +69,11 @@ namespace Lab4_GridGame
                         else if (col == 4 && row == 2 || col == 10 && row == 2)
                         {
                             MapGrid[row, col] = new Monster();
+                            Console.Write(buffer);
+                        }
+                        else if (row == 5 && col == 16)
+                        {
+                            MapGrid[row, col] = new Troll();
                             Console.Write(buffer);
                         }
                         else if (col == 5 && row == 5 && door1.DoorOpen == false)
@@ -121,7 +126,7 @@ namespace Lab4_GridGame
 
                 //Checks if the player is on a monster-tile
                 IsMonsterRoom();
-
+                TrollRoom();
                 //Switch statement to register player input and check whether or not the player is able to take a step.
                 var input = Console.ReadKey();
                 playerInput = (char)input.Key;
@@ -298,6 +303,18 @@ namespace Lab4_GridGame
             {
                 player.NumberOfTurns += 10;
                 Console.WriteLine("You enter a room and a monster attacks you. It takes a while to fight it.");
+            }
+        }
+        public void TrollRoom()
+        {
+            if ((MapGrid[player.PosRow, player.PosCol] is Troll && playerInput == 'W')||
+                (MapGrid[player.PosRow, player.PosCol] is Troll && playerInput == 'D') ||
+                (MapGrid[player.PosRow, player.PosCol] is Troll && playerInput == 'A') ||
+                (MapGrid[player.PosRow, player.PosCol] is Troll && playerInput == 'S')
+                )
+                {
+                player.NumberOfTurns += 60;
+                Console.WriteLine("You entered a room where the Troll is, it takes you a while to fight it.");
             }
         }
     }
